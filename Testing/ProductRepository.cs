067bb6135be3ea22213464b7198ca814627c54c4
Jessica <jessica1329b@gmail.com>
2022-04-27 15:22:33 -0400

    ASP.NET Intro

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository 
+    {
+        private readonly IDbConnection_ conn;
+        private object return_conn;
+        private IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products");
+        }
+    }
+}
