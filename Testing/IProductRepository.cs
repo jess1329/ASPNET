1668a0dc8d414ab54e6765434327f83dd785b5c8
Jessica <jessica1329b@gmail.com>
2022-05-05 19:11:31 -0400

    ASPNET part 2

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Testing.Models;
 
 namespace Testing
@@ -6,5 +9,6 @@ namespace Testing
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
     }
 }
