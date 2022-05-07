34d58a7cd9c612d00a400ac111b99aac199daec5
Jessica <jessica1329b@gmail.com>
2022-05-07 00:27:50 -0400

    ASPNET Update Product

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -6,9 +6,16 @@ using Testing.Models;
 
 namespace Testing
 {
-    public interface IProductRepository
+    public class ProductRepository : IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product); 
+        {
+         _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+     new {name = product.Name, price = product.Price, id = product.ProductID });
+
+
+        }
     }
 }
